44679982042ae4e0300755215284c932f3a52d5f
lb <863379105@qq.com>
2020-12-11 14:06:18 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  state={
+    showListFlag : false
+  }
+  showList = () => {
+    this.setState({
+      showListFlag : !this.state.showListFlag
+    })
+    console.log(this.state.showListFlag);
+  }
+  renderList(){
+    if(this.state.showListFlag){
+      return 'tab-list show-list'
+    }
+    return 'tab-list'
+  }
+  render(){
+    return(
+      <div>
+        <div className='tab-card'>
+          <div className='tab-name' onClick={this.showList}>Router</div>
+          <ul className={this.renderList()}>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+          </ul>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
